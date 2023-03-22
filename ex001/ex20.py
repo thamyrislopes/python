9d886596406d85749bfe14602c731af8f5085d99
Thamyris Lopes <101022477+thamyrislopes@users.noreply.github.com>
2023-03-21 21:19:13 -0300

    Create ex20.py

---
+++ ex001/ex20.py
@@ -0,0 +1,8 @@
+texto = "Meu NOME é Thamyris e sou estudante de Análise e Desenvolvimento de sistemas."
+print(f"Tamanho do texto = {len(texto)}")
+print(f"Thamyris in texto = {'Thamyris' in texto}")
+print(f"Quantidade de y no texto = {texto.count('y')}")
+print(f"As 03 primeiras letras são = {texto[0:3]}")
+print (texto.upper())
+print (texto.lower())
+print (texto.replace("s", 'W'))
